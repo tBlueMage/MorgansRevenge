08cc40a9d989b8e5d140551397aad8578af69c53
tBlueMage <47580410+tBlueMage@users.noreply.github.com>
2020-09-17 21:28:29 -0400

    Scene Switching
    We have scene switching between title, plains level, win, and lose screen. An actual build of the game also exits when the exit button is pressed.

---
+++ Morgan's Revenge/Assets/Scripts/ENDAPP_SCRIPT.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENDAPP_SCRIPT : MonoBehaviour
+{
+    //A public function that ends the application. It needs to be attached to something in the current scene in order to run.
+    public void endGame()
+    {
+        Application.Quit();
+    }
+
+}
