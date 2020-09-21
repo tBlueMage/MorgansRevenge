2d77dd50e9c0346d4014d523b4f3e6ee4ad844d2
kusmies <47580434+kusmies@users.noreply.github.com>
2020-09-21 17:39:53 -0400

    water

---
+++ Assets/water.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water : MonoBehaviour
+{
+ 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" || collision.tag == "Water")
+        {
+            CHASEN_SCRIPT.level.changeScene(2);
+        }
+    }
+}
