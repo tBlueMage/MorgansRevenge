331ea08bd718d11ea0a1105fb81a01335192812b
kusmies <47580434+kusmies@users.noreply.github.com>
2020-10-02 20:14:46 -0400

    dropcode

---
+++ Assets/enemydrops/enemydrop.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+public class enemydrop : MonoBehaviour
+{//how the dropprefab is called
+    //how the objects drop
+    public Transform spawn;
+    public GameObject Bsnack;
+    public GameObject Bmeal;
+    public GameObject Mvial;
+    public GameObject Mpotion;
+    public GameObject GoldCoin;
+    public GameObject CoinBag;
+    public GameObject GoldBar;
+    public GameObject playeref;
+    //player is called
+    Player players;
+    //int for the drop
+    public int Drop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //the range of the objects being dropped
+        Drop = Random.Range(1, 7);
+        
+        //how player is called
+        var player = GameObject.FindWithTag("Player");
+        players = player.GetComponentInParent<Player>();
+    }
+
+ 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //when the player collides with a test dummy drop an object from 1 to 7
+        if (collision.tag == "Player" || collision.tag == "Dummy")
+        {
+            if(Drop==1)
+            {
+                GameObject drop1;
+
+                drop1= (Instantiate(Bmeal, spawn.transform.position, transform.rotation)) as GameObject;
+                drop1.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0));
+
+            }
+            if (Drop == 2)
+            {
+                GameObject drop2;
+
+                drop2 = (Instantiate(Bsnack, spawn.transform.position, transform.rotation)) as GameObject;
+                drop2.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0));
+
+            }
+            if (Drop == 3)
+            {
+                GameObject drop3;
+
+                drop3 = (Instantiate(Mvial, spawn.transform.position, transform.rotation)) as GameObject;
+                drop3.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0));
+
+            }
+            if (Drop == 4)
+            {
+                GameObject drop4;
+                drop4 = (Instantiate(Mpotion, spawn.transform.position, transform.rotation)) as GameObject;
+                drop4.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0));
+
+            }
+
+            if (Drop == 5)
+            {
+                GameObject drop5;
+
+                drop5 = (Instantiate(GoldBar, spawn.transform.position, transform.rotation)) as GameObject;
+                drop5.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0));
+
+            }
+
+            if (Drop == 6)
+            {
+                GameObject drop6;
+                drop6 = (Instantiate(CoinBag, spawn.transform.position, transform.rotation)) as GameObject;
+                drop6.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0));
+
+            }
+            if (Drop == 7)
+            {
+                GameObject drop7;
+
+               drop7 = (Instantiate(GoldCoin, spawn.transform.position, transform.rotation)) as GameObject;
+                drop7.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0));
+
+            }
+            //resets the drop range for the item drop
+            Drop = Random.Range(1, 7);
+        }
+
+
+    }
+}
