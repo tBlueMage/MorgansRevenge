367e708b1ec1dfb9ed2394cf95cdedeb6f8cdd0d
kusmies <47580434+kusmies@users.noreply.github.com>
2020-10-06 14:02:00 -0400

    portedattacks

---
+++ Assets/Scripts/Spells/Fireball/Script/Fireball.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+
+   
+
+    public GameObject FireballPrefab;
+    public GameObject FireballSpawn;
+
+    public Player manacost;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+
+
+                if ((Input.GetKeyDown(KeyCode.O) &&!Input.GetKeyDown(KeyCode.S) && !Input.GetKeyDown(KeyCode.D) && !Input.GetKeyDown(KeyCode.W)))
+                {
+                    Fire();
+                }
+
+
+
+            
+
+    }
+    void Fire()
+    {
+        //have a bullet
+        GameObject blade;
+
+        Debug.Log("normalShot");
+
+        //make a bullet
+        blade = (Instantiate(FireballPrefab, FireballSpawn.transform.position, transform.rotation)) as GameObject;
+
+        //give it force
+        blade.GetComponent<Rigidbody2D>().AddForce(new Vector2(100, 0));
+
+        //destroy after 2 seconds
+        Destroy(blade, 1.0f);
+
+       
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        if (collision.tag == "Fireball" || collision.tag == "Soldier")
+        {
+
+            Destroy(gameObject);
+
+    }
+
+}
+
+   
+}
---
+++ Assets/Scripts/Spells/Lightning/Scripts/Lightning.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+
+    public GameObject LightningPrefab;
+    public GameObject LightningSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+   
+
+
+
+                if ((Input.GetKeyDown(KeyCode.O) && Input.GetKeyDown(KeyCode.D)))
+                {
+                    Fire();
+
+                }
+
+            
+
+            
+    }
+
+    void Fire()
+    {
+        //have a bullet
+        GameObject light;
+
+        Debug.Log("normalShot");
+
+        //make a bullet
+        light = (Instantiate(LightningPrefab, LightningSpawn.transform.position, transform.rotation)) as GameObject;
+
+        //give it force
+        light.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 0));
+
+        //destroy after 2 seconds
+
+      Destroy(light, 1.0f);
+
+    }
+
+
+    
+}
